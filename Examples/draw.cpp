7103790aa2cb022938ad7a6148dce0773730d67b
qPCR4vir <arielvina@yahoo.es>
2015-03-19 11:45:44 +0100

    Update draw.cpp

--- Examples/draw.cpp
+++ Examples/draw.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-    using namespace nana::gui;
+    using namespace nana;
 
     form fm;
     drawing dw(fm);
-    dw.draw([](nana::paint::graphics& graph)
+    dw.draw([](paint::graphics& graph)
     {
-        graph.rectangle(nana::rectangle(5, 5, 50, 50), 0xFF0000, true);
+        graph.rectangle(rectangle{5, 5, 50, 50}, 0xFF0000, true);
         graph.line(5, 5, 55, 55, 0xFFFFFF);
     });
 
@@ -16,28 +16,3 @@ int main()
     fm.show();
     exec();
 }
-
-//C++03
-//
-//#include <nana/gui/wvl.hpp>
-//
-//void draw_method(nana::paint::graphics& graph)
-//{
-//    graph.rectangle(nana::rectangle(5, 5, 10, 10), 0xFF0000, true);
-//}
-//
-//int main()
-//{
-//    using namespace nana::gui;
-//
-//    form fm;
-//    drawing dw(fm);
-//    dw.draw(draw_method);
-//
-//    dw.update();
-//    fm.show();
-//    exec();
-//}
-
-
-/// \todo docs: separate examples Cpp03 and 11 in differents directories: examples-Cpp03 and *11 for the exaples wich difier
