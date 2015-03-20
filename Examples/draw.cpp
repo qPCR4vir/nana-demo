07a4cd4d4919f9b4c5940051b8bb221e1131d32f
qPCR4vir <arielvina@yahoo.es>
2015-03-20 11:19:40 +0100

    star drawing :-)

--- Examples/draw.cpp
+++ Examples/draw.cpp
@@ -10,6 +10,13 @@ int main()
     {
         graph.rectangle(rectangle{5, 5, 50, 50}, true, colors::red );
         graph.line(point(5, 5), point(55, 55), colors::blue);
+        graph.line_begin(200,100);
+        graph.line_to(point(300,300));
+        graph.line_to(point(100,200));
+        graph.line_to(point(300,200));
+        graph.line_to(point(100,300));
+        graph.line_to(point(200,100));
+
     });
 
     dw.update();
