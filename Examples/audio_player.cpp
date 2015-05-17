d913e3e2c7dcf1b85d3f521e8fdaca1da02af03a
qPCR4vir <arielvina@yahoo.es>
2015-05-18 00:00:34 +0200

    audio tested

--- Examples/audio_player.cpp
+++ Examples/audio_player.cpp
@@ -1,4 +1,9 @@
 
+// Please, put a test audio file in "../Examples/audio1.wav"  in WAV PCM format
+//
+// you can donwload (an rename) an example from: 
+// http://download.wavetlan.com/SVV/Media/HTTP/WAV/NeroSoundTrax/NeroSoundTrax_test3_PCM_Mono_CBR_8SS_8000Hz.wav
+
 
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/button.hpp>
@@ -7,20 +12,26 @@
 
 int main()
 {
-    nana::audio::player player(STR("audio.wav"));
+    nana::audio::player player(STR("../Examples/audio1.wav"));
     nana::threads::pool pool(1); //Only 1 thread.
 
     using namespace nana;
 
     form fm;
-    button btn(fm, nana::rectangle(10, 10, 100, 24));
-    btn.caption(STR("Play the audio"));
+    button btn     (fm, nana::rectangle(10, 10, 180, 24));
+    button btn_wait(fm, nana::rectangle(10, 40, 180, 24));
+    btn     .caption(STR("Play the audio"));
+    btn_wait.caption(STR("Play and wait 5 min !!!!"));
 
     //C++11
     btn.events().click(pool_push(pool, [&]()
                            {
                                player.play();
                            }));
+    btn_wait.events().click([&]()
+                       {
+                         player.play();
+                       });
 
     fm.show();
 
