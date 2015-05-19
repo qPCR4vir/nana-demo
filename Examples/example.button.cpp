2ef8e2db505494e335a6f9729eb1c47bcadc9eb3
qPCR4vir <arielvina@yahoo.es>
2015-05-19 17:44:24 +0200

    button bground update v1.0.2

--- Examples/example.button.cpp
+++ Examples/example.button.cpp
@@ -1,12 +1,34 @@
+// http://nanapro.codeplex.com/discussions/544169
+
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/button.hpp>
+
 int main()
-		{
-		  nana::gui::form form;
-		  nana::gui::button btn(form, nana::rectangle(5, 5, 94, 23));
-		  btn.image(STR("button_image.bmp"));
-		  btn.image_valid_area(nana::arrange::horizontal, nana::rectangle(0, 0, 94 * 5, 23));
-		  form.show();
-		  nana::gui::exec();
-		}
-/*!\todo provide button_image.bmp*/
+{
+    using namespace nana;
+
+    form fm;
+
+    //Define a background for a button.
+    element::bground bground;
+
+    //Load the BMP file
+    //The second parameter is passed by true to indicate that each state aligns vertically.
+    //The third parameter specifies the valid area of the picture. It uses the whole picture when the width or height of the valid area is zero.
+    bground.image(L"../Examples/bground.6states.bmp", true, {});
+
+    //Defaultly the button will stretch the whole background of state to fit the widget,
+    //but it's easy to make the border deformation occurred. Now we need to specified the pixels
+    //of 4 borders, and these border areas will not be deformed when stretching.
+    bground.stretch_parts(4, 4, 4, 4);  //left, top, right, bottom
+
+    //Create the button
+    button btn(fm, nana::rectangle(20, 20, 140, 40));
+
+    //Set the bground object.
+    btn.set_bground(bground);
+
+    fm.show();
+    exec();
+}
+
