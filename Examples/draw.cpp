f5834d4b4dcb177a22bcd84404c360a2a0f3f310
qPCR4vir <arielvina@yahoo.es>
2015-07-10 16:55:15 +0200

    image paste see: http://nanapro.org/en-us/thread.php?id=bcdeced054d90df3929abfafeb8300da&stmp=1436524264135

--- Examples/draw.cpp
+++ Examples/draw.cpp
@@ -16,9 +16,15 @@ int main()
         graph.line_to(point(300,200));
         graph.line_to(point(100,300));
         graph.line_to(point(200,100));
-
+		graph.save_as_file("graphics.bmp");
+		nana::paint::image img(STR("C:\\Prog\\ExtLib\\nana-demo\\bin\\test.bmp"));
+		img.paste(img.size(), graph, nana::point());
+		graph.save_as_file("C:\\Prog\\ExtLib\\nana-demo\\bin\\testGraphics.bmp");
     });
-
+	nana::paint::graphics graphics;
+	nana::paint::image img(STR("C:\\Prog\\ExtLib\\nana-demo\\bin\\test.bmp"));
+	img.paste(img.size(), graphics, nana::point());
+	graphics.save_as_file("C:\\Prog\\ExtLib\\nana-demo\\bin\\testGraphics2.bmp");
     dw.update();
     fm.show();
     ::nana::exec();
