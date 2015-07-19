c2add08ac459046f4554a82ac1430c10c03edf7a
qPCR4vir <arielvina@yahoo.es>
2015-07-19 02:47:30 +0200

    adapt to a group with only one panel

--- Examples/group.cpp
+++ Examples/group.cpp
@@ -7,23 +7,13 @@
 
 // This example need nana/gui/widgets/group.hpp and the corresponding group.cpp. 
 // It can be found in branch:
-// https://github.com/cnjinhao/nana/tree/feature-group
+// https://github.com/cnjinhao/nana/blob/develop/include/nana/gui/widgets/group.hpp
 // or in:
 // https://github.com/qPCR4vir/nana
 // for exapmle in: https://github.com/qPCR4vir/nana/blob/master/include/nana/gui/widgets/group.hpp
 // and: https://github.com/qPCR4vir/nana/blob/master/source/gui/widgets/group.cpp
 
 using namespace nana;
-
-template<typename T, typename ...Args>
-T* add_widget(Args&&... args)
-{
-	widgets_.emplace_back(new T(*this, std::forward<Args>(args)...));
-
-	plc_["abc"] << widgets_.back()->handle();
-	plc_.collocate();
-	return static_cast<T*>(widgets_.back().get());
-}
 
 int main()
 {
@@ -42,42 +32,42 @@ int main()
     plc["all"]  << ext_gr;
 
     // the external group contain:
-    group grp_left {ext_gr.inner(), STR("A new <bold=true, color=0xff0000, font=\"Consolas\">Group:</>"), true };
-    group grp_right{ext_gr.inner(), STR("A right <bold=true, color=0xff0000, font=\"Consolas\">Group:</>"), true};
+    group grp_left {ext_gr, STR("A new <bold=true, color=0xff0000, font=\"Consolas\">Group:</>"), true };
+    group grp_right{ext_gr, STR("A right <bold=true, color=0xff0000, font=\"Consolas\">Group:</>"), true};
 
-    ext_gr.plc().div("horizontal gap=3 margin=20  < <left> | 70% <right>> ");
-    ext_gr.plc()["left"]  << grp_left;
-    ext_gr.plc()["right"] << grp_right;
+    ext_gr.div("horizontal gap=3 margin=20  < <left> | 70% <right>> ");
+    ext_gr["left"]  << grp_left;
+    ext_gr["right"] << grp_right;
 
     // the left group
-    grp_left.plc().div("buttons vert gap=5 margin=3");
+    grp_left.div("buttons vert gap=5 margin=3");
 
 
     // the right group
-    group nested(grp_right.inner() );
-    label  lab {grp_right.inner(), STR("A simple label ")};
-    button b1  {grp_right.inner(), STR("add button")};
+    group nested(grp_right.handle());
+    label  lab {grp_right, STR("A simple label ")};
+    button b1  {grp_right, STR("add button")};
 	b1.events().click([&grp_left, &btns]
 	{
-		btns.emplace_back(new button( grp_left.inner(), STR("Button") ));
+		btns.emplace_back(new button( grp_left, STR("Button") ));
 
-		grp_left.plc()["buttons"] << *btns.back();
-		grp_left.plc().collocate();
+		grp_left["buttons"] << *btns.back();
+		grp_left.collocate();
 	});
-    button b2  {grp_right.inner(), STR("button2")};
-    button b3  {grp_right.inner(), STR("button3")};
-    grp_right.plc().div( "<vertical margin=2 gap= 2 <vert lab> | 40% < <left> | 70% <right>> >");
-    grp_right.plc()["lab"]  << lab.text_align(align::right) << nested ;
-    grp_right.plc()["left"] << b1  ;
-    grp_right.plc()["right"]<< b2 << b3;
+    button b2  {grp_right, STR("button2")};
+    button b3  {grp_right, STR("button3")};
+    grp_right.div( "<vertical margin=2 gap= 2 <vert lab> | 40% < <left> | 70% <right>> >");
+    grp_right["lab"]  << lab.text_align(align::right) << nested ;
+    grp_right["left"] << b1  ;
+    grp_right["right"]<< b2 << b3;
 
     // the nested (rigth up) group
-    label lab1{nested.inner(), STR("A very simple group:")};
-    button b4{nested.inner(), STR("button4")};
-    button b5{nested.inner(), STR("button5")};
-    button b6{nested.inner(), STR("button6")};
-    nested.plc().div( " margin=3 min=30 gap= 2 all");
-    nested.plc()["all"]  << lab1 << b4 << b5 << b6;
+    label lab1{nested, STR("A very simple group:")};
+    button b4{nested, STR("button4")};
+    button b5{nested, STR("button5")};
+    button b6{nested, STR("button6")};
+    nested.div( " margin=3 min=30 gap= 2 all");
+    nested["all"]  << lab1 << b4 << b5 << b6;
 
 
     plc.collocate();
