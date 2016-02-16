2a12afc39b54a6f34d12bf706451a00f4b6bc7b4
Ariel Vina-Rodriguez <arielvina@yahoo.es>
2016-02-16 21:56:23 +0100

    ifdef    NANA_ENABLE_PNG

--- Examples/png.cpp
+++ Examples/png.cpp
@@ -6,10 +6,16 @@ int main()
     using namespace nana;
     form fm;
     picture pic(fm);
-	place p{fm};
+    place p{fm};
     p.div("pic"); 
-	p["pic"] << pic ;
-    pic.load( "../Examples/a_png_file.png" );
+    p["pic"] << pic ;
+#ifdef    NANA_ENABLE_PNG
+    pic.load( paint::image("../Examples/drawing.png") );
+#elifdef   NANA_ENABLE_JPGG     
+   // pic.load( paint::image("../Examples/drawing.jpg") );
+#else 
+    pic.load( paint::image("../Examples/Save.bmp") );
+#endif
     fm.show();
     exec();
 }
