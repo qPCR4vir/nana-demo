2d5d3bc1e0a1e4ee034aef77eb765993a4256434
qPCR4vir <arielvina@yahoo.es>
2016-03-01 18:53:05 +0100

    test 3 time click

--- Examples/clicked.cpp
+++ Examples/clicked.cpp
@@ -8,11 +8,23 @@ void clicked(const nana::arg_click & eventinfo)
      std::cout<<  "When the window  fm  is clicked, this function is called. \n";
 }
 
+
+
 int main()
 {
     using namespace nana;
     form fm;
     fm.events().click(clicked);
     fm.show();
-    exec();
+	exec(10, [&fm]() 
+		{
+			std::cout << "3 times automatic click. \n";
+			fm.events().click.emit(nana::arg_click{});
+			fm.events().click.emit(nana::arg_click{});
+			fm.events().click.emit(nana::arg_click{});
+			// char c;
+			// std::cin >> c;
+
+			fm.close(); 
+	     });
 }
