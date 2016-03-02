8f694f0b9c68ac6e3b8e04f67c1a9280feb9c1da
qPCR4vir <arielvina@yahoo.es>
2016-03-02 16:35:39 +0100

    pending with then mouse.

--- Examples/clicked.cpp
+++ Examples/clicked.cpp
@@ -1,7 +1,10 @@
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/button.hpp>
 
-#include <iostream> 
+#include <iostream>
+#include "../../nana/include/nana/gui/detail/general_events.hpp"
+#include "../../nana/include/nana/gui/basis.hpp"
+#include "../../nana/include/nana/gui/wvl.hpp"
 
 void clicked(const nana::arg_click & eventinfo)
 {
@@ -16,15 +19,28 @@ int main()
     form fm;
     fm.events().click(clicked);
     fm.show();
-	exec(10, [&fm]() 
+	exec(2, [&fm]()
 		{
 			std::cout << "3 times automatic click. \n";
 			fm.events().click.emit(nana::arg_click{});
 			fm.events().click.emit(nana::arg_click{});
 			fm.events().click.emit(nana::arg_click{});
+
+			nana::arg_mouse m;
+			m.window_handle=fm;
+			m.alt=m.ctrl=m.mid_button=m.shift=false;
+			m.left_button=true;
+			m.pos.x=m.pos.y=1;
+			m.button=::nana::mouse::left_button;
+
+			std::cout << "Now with then mouse. \n";
+			//fm.events().mouse_down.emit(m);
+			//fm.events().mouse_up.emit(m);
+
 			// char c;
 			// std::cin >> c;
 
-			fm.close(); 
-	     });
+			//fm.close();
+
+	     }, 1, &fm);
 }
