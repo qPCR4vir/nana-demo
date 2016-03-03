fa5adf4aa09e6ea04cd7195c0a63e18cafa95a7c
Ariel Vina-Rodriguez <arielvina@yahoo.es>
2016-03-03 14:53:29 +0100

    Update main.cpp

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -7,7 +7,11 @@ int main()
     using namespace nana;
     std::cout << "Hello main... and nana/gui/wvl.hpp " << std::endl;    
     try {
-    form fm;
+            form fm;
+            std::cout << "After fm " << std::endl; 
+            //fm.events().click(clicked);
+            fm.show();
+            std::cout << "After fm.show() " << std::endl;  
     }
     catch (std::exception& e)
     {
@@ -17,8 +21,5 @@ int main()
     {
         std::cout << "Exception occurred" << std::endl; 
     }
-    std::cout << "After fm " << std::endl; 
-    //fm.events().click(clicked);
-    fm.show();
-    std::cout << "After fm.show() " << std::endl;  
+
 }
