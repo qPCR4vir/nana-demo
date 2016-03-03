74b23ed0b37dd47e78fe5c913b8ee09361ca6285
Ariel Vina-Rodriguez <arielvina@yahoo.es>
2016-03-03 16:10:19 +0100

    Update decore.cpp

--- Examples/decore.cpp
+++ Examples/decore.cpp
@@ -4,5 +4,5 @@ int main()
     using namespace nana; 
     form fm(API::make_center(240, 160),  appear::decorate<appear::minimize, appear::maximize>()); 
     fm.show(); 
-    exec(); 
+    exec(1, [](){},1,&fm);
 } 
