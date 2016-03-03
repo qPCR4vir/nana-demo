a45c0ddbd72c150db0469c3ed95fb7182beea6d3
Ariel Vina-Rodriguez <arielvina@yahoo.es>
2016-03-03 12:57:51 +0100

    nana form  fm.show()

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -2,5 +2,11 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Hello main... and nana/gui/wvl.hpp ";
+    using namespace nana;
+    std::cout << "Hello main... and nana/gui/wvl.hpp ";    
+    form fm;
+    std::cout << "After fm ";    
+    //fm.events().click(clicked);
+    fm.show();
+    std::cout << "After fm.show() ";    
 }
