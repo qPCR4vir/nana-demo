aa3c328a7d49fcf3060dcecfe23c1149cfdf71d4
qPCR4vir <arielvina@yahoo.es>
2018-10-01 19:40:14 +0200

    trying to fix a definition duplication of boolean window.h vs jpeg

--- Examples/png.cpp
+++ Examples/png.cpp
@@ -5,17 +5,27 @@ int main()
 {
     using namespace nana;
     form fm;
-    picture pic(fm);
+    picture picPNG  {fm},
+            picJPEG {fm},
+            picBMP  {fm};
+
     place p{fm};
     p.div("pic"); 
-    p["pic"] << pic ;
-#ifdef    NANA_ENABLE_PNG
-    pic.load( paint::image("../Examples/drawing.png") );
-#elifdef   NANA_ENABLE_JPGG     
-   // pic.load( paint::image("../Examples/drawing.jpg") );
-#else 
-    pic.load( paint::image("../Examples/Save.bmp") );
+
+#ifdef  NANA_ENABLE_PNG
+    picPNG.load( paint::image("../Examples/png.png") );
+    p["pic"] << picPNG ;
 #endif
+
+#ifdef  NANA_ENABLE_JPEG
+   // picJPEG.load( paint::image("../Examples/jpeg.jpg") );
+    p["pic"] << picJPEG ;
+#endif
+
+    picBMP.load( paint::image("../Examples/bmp.bmp") );
+    p["pic"] << picBMP ;
+    p.collocate();
+
     fm.show();
     exec();
 }
