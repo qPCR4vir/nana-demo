8edb4e365d5b1c404c0338d930c8b1cba3c7f61a
qPCR4vir <arielvina@yahoo.es>
2024-05-23 18:36:43 +0200

    fail if no pictures found

--- Examples/animate-bmp.cpp
+++ Examples/animate-bmp.cpp
@@ -1,3 +1,6 @@
+#include <exception>
+#include <iostream>
+
 #include <nana/gui.hpp>
 #include <nana/gui/animation.hpp>
 
@@ -7,20 +10,40 @@ int main()
    
     //Build frames
     frameset fset;
+
+    // please put the images in the "../Examples/" folder
     auto grs = {"000", "015", "030", "045", "060", "075", "090", "105", "120", "135", "150", "170",
                 "180", "195", "210", "225", "240", "260", "270", "285", "300", "315", "330", "345"};
-    for (const auto& gr: grs)
-        fset.push_back(nana::paint::image(std::string("../Examples/a_pic")+gr+".bmp"));
+    
+    try
+    {   
+        for (const auto& gr: grs)
+        {
+            auto filename = std::string("../Examples/a_pic") + gr + ".bmp";
+            nana::paint::image img(filename);
 
-    //A widget to display animation.
-    form fm;
-    fm.show();
+            if (img.empty()) throw std::runtime_error("Failed to load image: " + filename);
 
-    animation ani;
-    ani.push_back(fset);
-    ani.output(fm, nana::point());
-    ani.looped(true);
-    ani.play();
+            fset.push_back(nana::paint::image(std::string("../Examples/a_pic") + gr + ".bmp"));
+        }
+        //A widget to display animation.
+        form fm;
+        fm.show();
 
-    exec();
+        animation ani;
+        ani.push_back(fset);
+        ani.output(fm, nana::point());
+        ani.looped(true);
+        ani.play();
+
+        exec();
+
+    } catch ( std::exception& e )
+    {
+        std::cerr << e.what();
+        // std::getchar();
+        return -1;
+    }
+
+    return 0;
 }
